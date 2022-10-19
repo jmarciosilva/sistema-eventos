74252401ef5856ddf74ba7dba88f6de691c4c27e
Jose Marcio Ferreira da Silva <jmarciosilva@gmail.com>
2022-10-19 19:50:15 -0300

    Aplicando Layouts e configurando as rotas

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contatos</title>
+</head>
+<body>
+    <h2>Página de Contatos</h2>
+    <a href="./" >Voltar</a>
+</body>
+</html>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Tela de Produtos - JMarcio')
+
+@section('content')
+
+    <h2>Página de Produtos</h2>
+    <a href="./" >Voltar</a>
+
+@endsection
